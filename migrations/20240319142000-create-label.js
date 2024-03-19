9128b1f76c3d209c464336d5a2846f28b111cc89
bernmp-dev <bernardomp.dev@gmail.com>
2024-03-19 13:41:43 -0300

    feat: Labels n Destinations migrations

---
+++ migrations/20240319142000-create-label.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/** @type { import('sequelize-cli').Migration } */
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Labels', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        field: 'user_id',
+        references: {
+          model: 'users',
+          key: 'id'
+        }
+      },
+      label: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Labels');
+  }
+};
