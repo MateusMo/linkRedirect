0fe5bc8276b6780ed9fa24dd156df00291c2e297
bernmp-dev <bernardomp.dev@gmail.com>
2024-03-22 21:23:37 -0300

    [loginController]fix: redirecionamento para rota /home .

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -3,19 +3,15 @@ const bcrypt = require('bcrypt');
 
 const post = async (req, res) => {
     const { nickName, password } = req.body;
+
     try {
-        const user = await User.findOne({
-            where: {
-                nickName: nickName
-            }
-        });
+        const user = await User.findOne({ where: { nickName } });
 
         if (user) {
             const passwordMatch = await bcrypt.compare(password, user.password);
             if (passwordMatch) {
-                //salva usuário na sessão
                 req.session.user = user.dataValues;
-                return res.status(200).render('home',  { user: req.session.user });
+                return res.status(200).redirect('/home');
             } else {
                 return res.status(401).render('login', { errorMessage: 'Senha incorreta' });
             }
@@ -31,7 +27,5 @@ const post = async (req, res) => {
 const get = async(req,res)=>{
     res.render('login')
 }
-
-
 
 module.exports = { post, get };
